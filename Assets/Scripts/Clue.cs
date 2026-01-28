21351ee852e81ef7e6fb50f057e5b39d088eaa03
ar4lym <s10267170@connect.np.edu.sg>
2026-01-28 12:01:10 +0800

    Implementation of Clue

--- Assets/Scripts/Clue.cs
+++ Assets/Scripts/Clue.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;  // required for XR events
 
 public class Clue : MonoBehaviour {
-    public TextMeshProUGUI clueText; // assign in Inspector
-    private bool found = false;
+    public TextMeshProUGUI clueText;   // assign in Inspector
+    private bool clueFound = false;
 
-    private void OnTriggerEnter(Collider other) {
-        if (!found && other.CompareTag("PlayerHand")) {
-            found = true;
-            clueText.gameObject.SetActive(true); // show floating text
-            clueText.text = "This looks carefully arranged...";
-            GameManager.Instance.FoundClue();
+    void Start() {
+        if (clueText != null) {
+            clueText.gameObject.SetActive(false); // start hidden
+        }
+    }
+
+    // Hook this up in Inspector to XR Simple Interactable events
+    public void InspectClue() {
+        if (!clueFound) {
+            clueFound = true;
+
+            if (clueText != null) {
+                clueText.gameObject.SetActive(true); // show text
+            }
+
+            if (gameObject.CompareTag("Clue1")) {
+                GameManager.Instance.RegisterClueFound(1);
+            } else if (gameObject.CompareTag("Clue2")) {
+                GameManager.Instance.RegisterClueFound(2);
+            } else if (gameObject.CompareTag("Clue3")) {
+                GameManager.Instance.RegisterClueFound(3);
+            }
+        }
+    }
+
+    public void StopInspecting() {
+        if (clueText != null) {
+            clueText.gameObject.SetActive(false); // hide text
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
-    private int cluesFound = 0;
+
+    private bool clue1Found = false;
+    private bool clue2Found = false;
+    private bool clue3Found = false;
 
     void Awake() {
-        Instance = this;
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Destroy(gameObject);
+        }
     }
 
-    public void FoundClue() {
-        cluesFound++;
-        Debug.Log("Clues found: " + cluesFound);
-        if (cluesFound >= 3) {
-            Debug.Log("Enough clues! You can identify the lost child.");
+    public void RegisterClueFound(int clueID) {
+        switch (clueID) {
+            case 1: clue1Found = true; break;
+            case 2: clue2Found = true; break;
+            case 3: clue3Found = true; break;
         }
+
+        Debug.Log($"Clue {clueID} found!");
+
+        if (clue1Found && clue2Found && clue3Found) {
+            UnlockChildren();
+        }
+    }
+
+    void UnlockChildren() {
+        Debug.Log("All clues found! Children interaction unlocked.");
+        // Later: enable NPC scripts here
     }
 }
