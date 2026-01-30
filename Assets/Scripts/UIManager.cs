8a281ece1707678534f8ad52df1ce7f6c60609ee
s10269187 <s10269187@connect.np.edu.sg>
2026-01-30 17:47:47 +0800

    Login works but use LOgin2

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -31,10 +31,10 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        ShowLogin();
-    }
+    // void Start()
+    // {
+    //     ShowLogin();
+    // }
 
     // public void ShowGame()
     // {
