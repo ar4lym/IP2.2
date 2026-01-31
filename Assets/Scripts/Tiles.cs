4758a79b500c4de743f0c5e8ec194239809908bb
s10269187 <s10269187@connect.np.edu.sg>
2026-02-01 02:20:10 +0800

    Tiles script wip

---
+++ Assets/Scripts/Tiles.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Tiles : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    //public AudioSource lava;
+    
+    /// <summary>
+    /// When player touch it, they respawn to spawn point
+    /// </summary>
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+
+            if (spawnPoint != null)
+            {
+                other.transform.position = spawnPoint.position;
+                Debug.Log("Teleporting to: " + spawnPoint.position);
+                lava.Play();
+
+                if (rb != null)
+                {
+                    rb.linearVelocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                    rb.Sleep();
+                }
+
+                Physics.SyncTransforms();
+            }
+            else
+            {
+                Debug.LogWarning("Spawn location not assigned!");
+            }
+        }
+
+
+    }
+
+}
