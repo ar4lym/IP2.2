34f49969860e8b1cbec51d9d8ff3f29a5e42a9ff
ar4lym <s10267170@connect.np.edu.sg>
2026-02-02 10:34:21 +0800

    Implementation of Scene Variation

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.XR.Interaction.Toolkit;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
-
-    void Awake()
+    public void Sceneloader()
     {
-        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
-        interactable.selectEntered.AddListener(OnSelected);
-    }
-
-    private void OnSelected(SelectEnterEventArgs args)
-    {
-        Debug.Log("Loading scene: " + sceneName);
         SceneManager.LoadScene(sceneName);
     }
 }
