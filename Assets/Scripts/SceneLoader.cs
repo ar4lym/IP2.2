20ef6f0e3d14005205ef58b16a52ddb921fee7e3
ar4lym <s10267170@connect.np.edu.sg>
2026-02-03 14:29:19 +0800

    Implementation of scene switching

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+
     public void Sceneloader()
     {
         SceneManager.LoadScene(sceneName);
+        StartCoroutine(SendSceneData(sceneName));
+    }
+
+    private IEnumerator SendSceneData(string scene)
+    {
+        string url = "https://yourserver.com/api/scene"; // Replace with your backend endpoint
+        WWWForm form = new WWWForm();
+        form.AddField("sceneName", scene);
+        form.AddField("timestamp", System.DateTime.UtcNow.ToString("o"));
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error sending scene data: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Scene data sent successfully!");
+            }
+        }
     }
 }
