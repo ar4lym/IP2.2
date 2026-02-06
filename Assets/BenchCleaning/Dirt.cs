ae5ac1c75f63d7ae7caf30570806c076ea674516
schanelle26 <s10269101@connect.np.edu.sg>
2026-02-06 13:56:02 +0800

    Bench dirt script update

--- Assets/BenchCleaning/Dirt.cs
+++ Assets/BenchCleaning/Dirt.cs
@@ -1,15 +1,38 @@
-using UnityEngine;
+// using UnityEngine;
+
+// public class Dirt : MonoBehaviour
+// {
+//     public float dirtAmount = 100f;
+
+//     public void Clean(float amount)
+//     {
+//         dirtAmount -= amount;
+
+//         if (dirtAmount <= 0)
+//         {
+//             gameObject.SetActive(false);
+//         }
+//     }
+// }
+
 
 public class Dirt : MonoBehaviour
 {
     public float dirtAmount = 100f;
+    private bool isCleaned = false;
 
     public void Clean(float amount)
     {
+        if (isCleaned) return;
+
         dirtAmount -= amount;
 
         if (dirtAmount <= 0)
         {
+            isCleaned = true;
+
+            BenchGameManager.Instance.BenchCleaned();
+
             gameObject.SetActive(false);
         }
     }
